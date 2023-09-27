9cc882e3dc812340ce667c56849253cccb7c504f
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2023-09-27 11:49:27 +0300

    ready

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,8 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    screens: {
-      sm: "540px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
     fontSize: {
       xs: [
         "0.75rem",
@@ -90,34 +83,114 @@ module.exports = {
           lineHeight: "1",
         },
       ],
+      "10xl": [
+        "10rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "11xl": [
+        "12rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "12xl": [
+        "14rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "13xl": [
+        "16rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "14xl": [
+        "20rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "15xl": [
+        "24rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "16xl": [
+        "30rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "17xl": [
+        "40rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "18xl": [
+        "50rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "19xl": [
+        "60rem",
+        {
+          lineHeight: "1",
+        },
+      ],
+      "20xl": [
+        "70rem",
+        {
+          lineHeight: "1",
+        },
+      ],
     },
     extend: {
-
+      animation: {
+        marquee: "marquee 12s linear infinite",
+        marquee2: "marquee 30s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": {
+            transform: "translateX(0%)",
+          },
+          "100%": {
+            transform: "translateX(-100%)",
+          },
+        },
+      },
       colors: {
-
-        blue: {
-         50: "#EBEBFF",
-      100: "#D2D2FE",
-      200: "#A6A4FE",
-      300: "#7E7CFD",
-      400: "#524FFD",
-      500: "#2522FC",
-      600: "#0703E2",
-      700: "#0502AB",
-      800: "#03026F",
-      900: "#020137",
-      950: "#01001E"
+        black: "#010101",
+        accent: {
+          '50': 'hsl(22, 100%, 96%)',
+    '100': 'hsl(25, 100%, 91%)',
+    '200': 'hsl(22, 100%, 82%)',
+    '300': 'hsl(20, 100%, 71%)',
+    '400': 'hsl(16, 100%, 60%)',
+    '500': 'hsl(14, 100%, 52%)',
+    '600': 'hsl(9, 100%, 50%)',
+    '700': 'hsl(6, 98%, 40%)',
+    '800': 'hsl(4, 87%, 34%)',
+    '900': 'hsl(4, 83%, 28%)',
+    '950': 'hsl(1, 89%, 15%)',
         },
       },
       fontFamily: {
+        serif: ["Newsreader", ...defaultTheme.fontFamily.serif],
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
         mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
       },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
+};
