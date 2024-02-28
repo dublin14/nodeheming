e89605c7f2055215225bae66788c18483eee56f9
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-02-28 19:53:54 +0200

    added content collection and cleaned up

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-   
+
     extend: {
       animation: {
         marquee: "marquee 12s linear infinite",
@@ -23,17 +23,17 @@ module.exports = {
       colors: {
         black: "#010101",
         accent: {
-          '50': 'hsl(22, 100%, 96%)',
-    '100': 'hsl(25, 100%, 91%)',
-    '200': 'hsl(22, 100%, 82%)',
-    '300': 'hsl(20, 100%, 71%)',
-    '400': 'hsl(16, 100%, 60%)',
-    '500': 'hsl(14, 100%, 52%)',
-    '600': 'hsl(9, 100%, 50%)',
-    '700': 'hsl(6, 98%, 40%)',
-    '800': 'hsl(4, 87%, 34%)',
-    '900': 'hsl(4, 83%, 28%)',
-    '950': 'hsl(1, 89%, 15%)',
+         50: "hsl(27, 100%, 96%)",
+          100: "hsl(29, 100%, 92%)",
+          200: "hsl(26, 100%, 83%)",
+          300: "hsl(25, 100%, 72%)",
+          400: "hsl(21, 100%, 61%)",
+          500: "hsl(19, 100%, 55%)",
+          600: "hsl(15, 95%, 48%)",
+          700: "hsl(12, 93%, 40%)",
+          800: "hsl(9, 84%, 34%)",
+          900: "hsl(10, 79%, 28%)",
+          950: "hsl(8, 86%, 15%)",
         },
       },
       fontFamily: {
